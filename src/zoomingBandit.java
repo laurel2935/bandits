06aec538c5bfaa96ab9354e28601342ad4b1a4d9
froggy618157725 <froggy618157725@59605304-12bf-11df-94ac-a1a109428b18>
2010-02-13 11:07:50 +0000

    Started the zooming algorithm

---
+++ src/zoomingBandit.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+
+
+public class zoomingBandit extends RealBandit {
+	
+	private class Arm{
+		private double identity;
+		private int n = 0;
+		private double r = 0;
+		public Arm(double input){
+			identity = input;
+		}
+		public double getIdentity(){
+			return identity;
+		}
+		
+		// Called to indicate that the arm was played
+		public void play(double reward){
+			n++;
+			r+=reward;
+		}
+		
+		//The average reward
+		public double mt(){
+			return r/n;			
+		}
+		
+		//The number of times the arm has been played in a phase
+		public double nt(){
+			return n;			
+		}
+		
+		// the confidence radius for this arm
+		public double rt(){
+			return Math.sqrt(8*iph/(2+nt()));
+		}
+	}
+	
+	private int iph = 1; //phase i
+	
+	// The list of active arms, as chosen by the oracle
+	// I'm not yet sure how we're representing our arms
+	private ArrayList<Arm> active;
+	
+	@Override
+	public double reward(double input) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	//The current average reward for an active arm
+	private double mt(Arm v){
+		return v.mt();
+	}
+	
+	//??????????????
+	//I think this needs to be able to cover everything with
+	//balls that shrink in radius each round.
+	public ArrayList<Arm> coveringOracle(){
+		return null;
+	}
+	
+	
+	// Index of a strategy (arm)
+	private double It(Arm v){
+		return v.mt()+2*v.rt();
+	}
+	public void runPhase(){		
+		for(int r = 0; r<Math.pow(2, iph);r++){
+			runRound();
+		}
+		iph ++;
+	}
+	private void runRound(){
+		// add active arms if necesssary
+		active = coveringOracle();
+		//Find the highest index arm
+		int maxI = 0;
+		double maxV = It(active.get(0));
+		for(int v = 1; v < active.size(); v++){
+			double nextIt = It(active.get(v));
+			if(nextIt>maxV){
+				maxI = v;
+				maxV = nextIt;
+			}				
+		}
+		// Play arm of highest index
+		active.get(maxI).play(reward(active.get(maxI).getIdentity()));
+	}
+}
