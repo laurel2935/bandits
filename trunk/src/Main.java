506323da2e4f918d747fc04ce126e9bc15e429d4
alphadoor <alphadoor@59605304-12bf-11df-94ac-a1a109428b18>
2010-02-14 06:40:22 +0000

    Added writing to file capabilities.

--- trunk/src/Main.java
+++ trunk/src/Main.java
@@ -8,7 +8,8 @@ public class Main {
         int arms = 100;
         DiscretizedUCB1 test_alg = new DiscretizedUCB1(test_prob.domain, arms);        
         int num_plays = 1000000;
-        Trial test_run = new Trial(test_alg, test_prob, num_plays); 
+        Trial test_run = new Trial(test_alg, test_prob, num_plays, "test1.txt"); 
+        test_run.setWriteInterval(1000);
         test_run.run();
     }
 
