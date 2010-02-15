4092e7eebea7cf008e4992c26ee61b2bb85b83ff
froggy618157725 <froggy618157725@59605304-12bf-11df-94ac-a1a109428b18>
2010-02-15 06:30:26 +0000

    Added Files for Oracle

---
+++ src/Link.java
@@ -0,0 +1,34 @@
+
+public class Link<T> {
+	private Link<T> prev;
+	private Link<T> next;
+	private T value;
+	
+	public Link(T contents){
+		value = contents;
+	}
+	
+	public void setNext(Link<T> n){
+		next = n;
+	}
+	
+	public void setPrev(Link<T> n){
+		prev = n;
+	}
+	public Link<T> getNext(){
+		return next;
+	}
+	
+	public Link<T> getPrev(){
+		return prev;
+	}
+	public void setValue(T val){
+		value = val;
+	}
+	public T getValue(){
+		return value;
+	}
+	public String toString(){
+		return (prev==null?"null":prev.getValue().toString()) + "->("+value.toString()+")->" +(next == null?"null":next.getValue().toString());
+	}
+}
