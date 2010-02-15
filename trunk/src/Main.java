89f4c0db29519a2ca562bbfd70feaec0d086160c
alphadoor <alphadoor@59605304-12bf-11df-94ac-a1a109428b18>
2010-02-15 07:20:39 +0000

    Added BinomialBandit and ComposedRealBandit.
    Apparently also modified the pdf

--- trunk/src/Main.java
+++ trunk/src/Main.java
@@ -2,15 +2,21 @@ public class Main {
 
     // Just testing stuff out for now.
     public static void main(String[] args) {
+        // Make bandit
         double[] coeffs = {0, 1};
-        PolynomialRealBandit test_prob = new PolynomialRealBandit(coeffs, 0, 1);
-        test_prob.setRewardMax(1);
-        NoisyBandit noisy = new NoisyBandit(test_prob, 1, 1);
-        
+        PolynomialRealBandit poly = new PolynomialRealBandit(coeffs, 0, 1);
+        poly.setRewardMax(1);
+        ComposedRealBandit comp = new ComposedRealBandit(poly, poly);
+        BinomialBandit bin = new BinomialBandit(comp);
+        NoisyBandit noise = new NoisyBandit(bin, .5, 1);
+
+        // Make algorithm
         int arms = 100;
-        Algorithm test_alg = new DiscretizedExp3(noisy.domain, arms);        
+        Algorithm test_alg = new DiscretizedUCB1(noise.domain, arms);        
+
+        // Run
         int num_plays = 1000000;
-        Trial test_run = new Trial(test_alg, test_prob, num_plays, "exp3.txt"); 
+        Trial test_run = new Trial(test_alg, noise, num_plays, "ucb3comp.txt"); 
         test_run.setWriteInterval(1000);
         test_run.run();
     }
