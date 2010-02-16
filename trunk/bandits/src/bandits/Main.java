bfe46e7feb9a055e67ca701d007f8a3585bcd11c
alphadoor <alphadoor@59605304-12bf-11df-94ac-a1a109428b18>
2010-02-16 02:49:00 +0000

    Finished adding what I'm going to add for the report.

--- trunk/bandits/src/bandits/Main.java
+++ trunk/bandits/src/bandits/Main.java
@@ -12,20 +12,19 @@ public class Main {
     // Just testing stuff out for now.
     public static void main(String[] args) {
         // Make bandit
-        double[] coeffs = {0, 1};
+        double[] coeffs = {1, -3, 3};
         PolynomialRealBandit poly = new PolynomialRealBandit(coeffs, 0, 1);
         poly.setRewardMax(1);
-//        ComposedRealBandit comp = new ComposedRealBandit(poly, poly);
-//        BinomialBandit bin = new BinomialBandit(comp);
-//        NoisyBandit noise = new NoisyBandit(bin, .5, 1);
+        BinomialBandit bin = new BinomialBandit(poly);
+        NoisyBandit noise = new NoisyBandit(bin, .5, 1);
 
         // Make algorithm
         int arms = 100;
-        Algorithm test_alg = new ZoomingAlgorithm(poly.domain);        
+        Algorithm test_alg = new DiscretizedEpsilonGreedy(noise.domain, arms);
 
         // Run
         int num_plays = 1000000;
-        Trial test_run = new Trial(test_alg, poly, num_plays, "random.txt"); 
+        Trial test_run = new Trial(test_alg, noise, num_plays, "e_greed.txt"); 
         test_run.setWriteInterval(1000);
         test_run.run();
     }
