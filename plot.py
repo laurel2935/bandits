0558f9babf3730362643e4577cd5fac3cc76d857
alphadoor <alphadoor@59605304-12bf-11df-94ac-a1a109428b18>
2010-02-16 05:51:45 +0000

    Fixed bug in plotting script.

--- plot.py
+++ plot.py
@@ -12,9 +12,14 @@ if len(sys.argv) <= 1:
     print usage
     quit()
 input_filename = sys.argv[1]
-output_filename = sys.argv[2] if len(sys.argv) >= 3 else \
-                  input_filename.split('.')[0]
-plot_title = sys.argv[3] if len(sys.argv) == 4 else output_filename
+if len(sys.argv) >= 3:
+    output_filename = sys.argv[2]
+else:
+    output_filename = input_filename.split('.')[0]
+if len(sys.argv) == 4:
+    plot_title = sys.argv[3]
+else:
+    plot_title = output_filename
 
 # Make the plot
 gplotter.plot(input_filename, output_filename, title=plot_title)
