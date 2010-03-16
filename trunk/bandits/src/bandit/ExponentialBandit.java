6454f0f2464c2779b76994223fabfea9ee8b74cc
mnfaulk <mnfaulk@59605304-12bf-11df-94ac-a1a109428b18>
2010-03-16 23:57:00 +0000

    Adding ExponentialBandit

---
+++ trunk/bandits/src/bandit/ExponentialBandit.java
@@ -0,0 +1,23 @@
+package bandit;
+
+import domain.DomainElement;
+import domain.RealNumber;
+
+public class ExponentialBandit extends RealBandit {
+	private final double mu;
+	private final double sigmaSq;
+	public ExponentialBandit(double mu, double sigmaSq){
+		super(0,1);
+		this.mu = mu;
+		this.sigmaSq = sigmaSq;
+	}
+
+	@Override
+	public double reward(DomainElement input) {
+		//ugly hack: casting...
+		double val = ((RealNumber)input).getValue(); // Value of input.
+		// TODO: check that x is in [0,1]
+		assert( (0 <= val) && (val <= 1 ));
+		return Math.exp( -Math.pow(val-mu,2)/(2*sigmaSq));
+	}
+}
